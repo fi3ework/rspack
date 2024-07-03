97c56a157cac15ccd9acb0abc428f08cac8c1106
fi3ework <fi3ework@gmail.com>
2024-07-03 14:09:03 +0800

    test: add test

--- tests/webpack-test/configCases/library/1-use-library/test.filter.js
+++ tests/webpack-test/configCases/library/1-use-library/test.filter.js
@@ -1,1 +1,4 @@
-module.exports = () => {return "https://github.com/web-infra-dev/rspack/issues/4313"}
+module.exports = () => {
+	return true;
+	// return "https://github.com/web-infra-dev/rspack/issues/4313";
+};
